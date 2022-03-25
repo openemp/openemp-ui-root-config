e45d85f7249c323e4459b927b4dfeffabf81b9d6
Ala Eddine Bouasla <bbbooo1994@gmail.com>
2022-03-25 14:45:21 +0100

    [FEAT] Init module with single SPA cli

--- webpack.config.js
+++ webpack.config.js
@@ -1,105 +1,28 @@
-const { DefinePlugin } = require("webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DotenvWebpackPlugin = require("dotenv-webpack");
 
-const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "openemp";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
 
-const path = require("path");
-
-require("dotenv").config();
-
-const __ServiceConfig__ = JSON.stringify(require("./service.conf.json"));
-const openempMf = JSON.stringify(require("./src/openemp.map.json"));
-const vendors = JSON.stringify(require("./src/vendors.map.json"));
-
-module.exports = (env) => {
-  const result = {
-    entry: path.resolve(__dirname, "src/root-config"),
-    output: {
-      filename: "openemp-root-config.js",
-      libraryTarget: "system",
-      path: path.resolve(__dirname, "dist"),
-    },
-    devtool: "source-map",
-    module: {
-      rules: [
-        { parser: { system: false } },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [{ loader: "babel-loader" }],
-        },
-      ],
-    },
-    devServer: {
-      historyApiFallback: true,
-      disableHostCheck: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-      },
-      hot: true,
-    },
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
     plugins: [
-      new DotenvWebpackPlugin(),
-      new DefinePlugin({
-        __ServiceConfig__,
-      }),
       new HtmlWebpackPlugin({
         inject: false,
         template: "src/index.ejs",
         templateParameters: {
-          isLocal: env && env.isLocal === "true",
-          openempMf,
-          vendors,
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
         },
       }),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackExternalsPlugin({
-        externals: [
-          {
-            module: "systemjs",
-            entry: [
-              "dist/system.min.js",
-              "dist/extras/amd.js",
-              "dist/extras/named-exports.min.js",
-            ],
-          },
-          {
-            module: "react",
-            entry: ["umd/react.development.js", "umd/react.production.min.js"],
-          },
-          {
-            module: "react-dom",
-            entry: [
-              "umd/react-dom.development.js",
-              "umd/react-dom.production.min.js",
-            ],
-          },
-          {
-            module: "@reach/router",
-            entry: "umd/reach-router.min.js",
-          },
-          {
-            module: "single-spa",
-            entry: "lib/system/single-spa.min.js",
-          },
-          {
-            module: "regenerator-runtime",
-            entry: "runtime.js",
-          },
-          {
-            module: "axios",
-            entry: "dist/axios.min.js",
-          },
-        ],
-        outputPath: "vendors",
-        enabled: true,
-      }),
     ],
-    externals: ["single-spa", /^@openemp\/.+$/],
-  };
-
-  return result;
+  });
 };
